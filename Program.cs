a98b1a24e0d068734c913c2cd5f90078ff4a5a86
RohitKokare810 <Rohitkokare810@gmail.com>
2023-11-08 16:15:24 +0530

    3rd pgm

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a number");
+            num = int.Parse(Console.ReadLine());
+            if(num==0)
+            {
+                Console.WriteLine("The Number is Zero");
+            }
+            else if (num > 0)
+            {
+
+                Console.WriteLine("The Number is Positive");
+                if(num%2==0)
+                {
+                    Console.WriteLine("Even Number");
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The Number is Negative");
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("Even Number");
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
